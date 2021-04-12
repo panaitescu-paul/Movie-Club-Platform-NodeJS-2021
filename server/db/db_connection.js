4866e560fffa0bb9ad1c0b24c0a46d01c6313542
Constantin <razvan1598@gmail.com>
2021-04-12 11:29:20 +0200

    Db connection

---
+++ server/db/db_connection.js
@@ -0,0 +1,16 @@
+const mysql      = require('mysql');
+const connection = mysql.createConnection({
+    host     : 'den1.mysql3.gear.host',
+    user     : 'movieclubdb',
+    password : 'Zm246A6W~-pX'
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('Error connecting: ' + err.stack);
+        return;
+    }
+
+    // console.log('Connected as id ' + connection.threadId);
+    console.log('Connected to database!');
+});
