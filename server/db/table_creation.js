859c9059be602f64e6d9275f2e3226762d089a02
Constantin <razvan1598@gmail.com>
2021-04-15 13:52:42 +0200

    Test

--- server/db/table_creation.js
+++ server/db/table_creation.js
@@ -1,11 +1,8 @@
 const conn = require("./db_connection");
 
-const roomTable = "CREATE TABLE IF NOT EXISTS Room(Id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, Name VARCHAR(255) NOT NULL)";
+const createRoomTable = "CREATE TABLE IF NOT EXISTS Room(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL)";
 
-conn.query(roomTable, function (err, result) {
+conn.query(createRoomTable, function (err, result) {
     if (err) throw err;
     console.log("Room Table created");
 });
-
-
-// test
