a0851ec7a4f48d9f68d8b9726813033ea448da2b
Constantin <razvan1598@gmail.com>
2021-04-22 14:34:43 +0200

    gateway created

---
+++ server/gateway/mcdb_gateway/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const gateway = require('express-gateway');
+require("../../api/admin");
+// require("../../api/user");
+require("../../api/room");
+require("../../api/message");
+require("../../api/participant");
+require("../../api/crew");
+// require("../../api/movie");
+// require("../../api/rating");
+// require("../../api/review");
+
+gateway()
+  .load(path.join(__dirname, 'config'))
+  .run();
