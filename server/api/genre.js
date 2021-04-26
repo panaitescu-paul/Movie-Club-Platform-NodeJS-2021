a31b9eda9f35bbe2035283626d52d817efb7490e
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2021-04-26 19:20:24 +0200

    Added new class: Genre class

---
+++ server/api/genre.js
@@ -0,0 +1,32 @@
+/**
+* Genre class
+*
+* @author Paul Panaitescu
+* @version 1.0 26 APR 2020
+*/
+
+const connection = require("../db/db_connection");
+const express = require("express");
+const axios = require('axios');
+const bcrypt = require('bcrypt');
+const HOSTNAME = 'localhost';
+const PORT = 3004;
+let app = express();
+app.use(express.json());
+
+// ******************************************************
+// ***                                                ***
+// ***             Genre CRUD Functionality           ***
+// ***                                                ***
+// ******************************************************
+
+
+// Server connection
+app.listen(PORT, HOSTNAME, (err) => {
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+    }
+});
