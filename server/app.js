44a1e4b60b7859d1136818e145981bc2d923e331
Constantin <razvan1598@gmail.com>
2021-04-28 11:39:32 +0200

    installed redis sessions example

--- server/app.js
+++ server/app.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const session = require('express-session');
 const path = require('path');
+const redis = require('redis');
+const bodyParser = require('body-parser');
+const redisStore = require('connect-redis')(session);
+const client  = redis.createClient();
 const HOSTNAME = 'localhost';
 const PORT = 3000;
 let app = express();
-app.use(express.json());
-app.use(session({secret: 'ssshhhhh',saveUninitialized: true, resave: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, '../client')));
-let sess;
+
+app.use(session({
+    secret: 'ssshhhhh',
+    // create new redis store.
+    store: new redisStore({ host: 'localhost', port: 6379, client: client,ttl : 260}),
+    saveUninitialized: false,
+    resave: false
+}));
 
 app.get('/',(req,res) => {
-    sess = req.session;
+    let sess = req.session;
     if(sess.email) {
         return res.redirect('/admin');
     }
@@ -18,31 +29,31 @@ app.get('/',(req,res) => {
 });
 
 app.post('/login',(req,res) => {
-    sess = req.session;
-    sess.email = req.body.email;
+    console.log(req.session);
+    req.session.email = req.body.email;
     res.end('done');
 });
 
 app.get('/admin',(req,res) => {
-    sess = req.session;
-    if(sess.email) {
-        res.write(`<h1>Hello ${sess.email} </h1><br>`);
+    console.log(req.session);
+    console.log(req.session.email);
+    if(req.session.email) {
+        res.write(`<h1>Hello ${req.session.email} </h1><br>`);
         res.end('<a href='+'/logout'+'>Logout</a>');
-    }
-    else {
+    } else {
         res.write('<h1>Please login first.</h1>');
         res.end('<a href='+'/'+'>Login</a>');
     }
 });
 
 app.get('/logout',(req,res) => {
+    console.log("here")
     req.session.destroy((err) => {
         if(err) {
             return console.log(err);
         }
         res.redirect('/');
     });
-
 });
 
 app.listen(PORT, HOSTNAME, (err) => {
