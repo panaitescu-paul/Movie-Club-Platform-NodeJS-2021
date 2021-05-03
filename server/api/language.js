bc82332cb61511ffae245bc4ec6eb75a1cda3d1b
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2021-05-03 16:49:37 +0200

    Added new method to the Languages class: READ Language by id

--- server/api/language.js
+++ server/api/language.js
@@ -42,6 +42,36 @@ app.get("/language", (req, res) => {
         }
     });
 });
+
+/**
+* READ Language by id
+*
+* Input:    id of the Language
+* Output:   an Language and their information,
+* Errors:   Language with this ID does not exist!
+*/
+app.get("/language/:id", (req, res) => {
+    console.log("req.params.id: ", req.params.id);
+    let sql = `SELECT * FROM language WHERE id = ?`;
+
+    connection.query(sql, [req.params.id], function(err, language) {
+        if (err) {
+            res.status(400).json({
+                error: err.message
+            });
+            console.log(err);
+        } else {
+            if(language.length) {
+                res.status(200).send(language[0]);
+            } else {
+                res.status(404).json({
+                    message: `Language with this ID (${req.params.id}) does not exist!`
+                });
+            }
+        }
+    });
+});
+
 // Server connection
 app.listen(PORT, HOSTNAME, (err) => {
     if(err){
