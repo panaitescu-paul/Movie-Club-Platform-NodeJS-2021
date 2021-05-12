ace66c9a908553d5e72b87013a33bb0cb35dac4a
Constantin <razvan1598@gmail.com>
2021-05-12 21:05:35 +0200

    Added rating stars hovering

--- client/js/functions.js
+++ client/js/functions.js
@@ -249,12 +249,27 @@ function ratingStarsSelection() {
     console.log(movieId);
     printRatingResult(ratingResult);
 
+
+
+
     function executeRating(stars, result) {
         const starClassActive = "rating__star fas fa-star";
         const starClassUnactive = "rating__star far fa-star";
         const starsLength = stars.length;
         let i;
+
         stars.map((star) => {
+
+            star.onmouseover = () => {
+                i = stars.indexOf(star);
+
+                if (star.className.indexOf(starClassUnactive) !== -1) {
+                    for (i; i >= 0; --i) stars[i].className = starClassActive;
+                } else {
+                    for (i; i < starsLength; ++i) stars[i].className = starClassUnactive;
+                }
+            };
+
             star.onclick = () => {
                 i = stars.indexOf(star);
 
